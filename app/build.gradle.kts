1958ec1582780ed8e85c62b6569339c0de72b950
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-07-16 10:02:17 +0900

    build: bump versionCode to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "sh.atman.beam"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "0.1.0"
     }
 
